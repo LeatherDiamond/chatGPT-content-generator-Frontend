d5d1453aa6f88c8d9fa0eabd69e62171c132d90e
Aliaksandr <alexander.dovguchits@gmail.com>
2023-08-30 18:01:34 +0200

    changes for deployment

--- src/components/GeneratedResponseForm/GeneratedResponseForm.jsx
+++ src/components/GeneratedResponseForm/GeneratedResponseForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Layout from '../Layout/Layout';
-import Lottie from 'lottie-react';
+import Lottie from 'react-lottie';
 import animationData from './pencil_animation.json';
 import RobotPointing from './robot-pointing.avif';
 import RobotImage from '../GeneratedImageForm/robot.png';
