2c119603639a3795b426c0f990318973247c2828
Aliaksandr <alexander.dovguchits@gmail.com>
2023-08-30 20:51:24 +0200

    make amends for deployment

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -6,7 +6,7 @@ function Layout({ children }) {
         <header>
           <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
             <div className="container-fluid">
-              <a className="navbar-brand" href="/"><img src="logo-image.png" alt="Logo" className='logo-image'/></a><br />
+              <a className="navbar-brand" href="/#//"><img src="logo-image.png" alt="Logo" className='logo-image'/></a><br />
               <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
               </button>
