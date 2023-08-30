9ea4891de86587b036e75217338021335556eb52
Aliaksandr Dauhuchyts <alexander.dovguchits@gmail.com>
2023-08-30 08:01:20 +0200

    Design completed

--- src/components/StartPage/StartPage.jsx
+++ src/components/StartPage/StartPage.jsx
@@ -6,8 +6,8 @@ import './StartPage.css';
 
 function StartPage() {
   const buttons = [
-    { text: 'Get Started with Text Generation', color: 'primary' },
-    { text: 'Get Started with Image Generation', color: 'secondary' },
+    { text: 'Get Started with Text Generation', color: 'primary', link: '/generate-response' },
+    { text: 'Get Started with Image Generation', color: 'secondary', link: '/generate-image' },
   ];
 
   const trail = useTrail(buttons.length, {
@@ -20,12 +20,12 @@ function StartPage() {
   return (
     <Layout>
       <div className="start-page-container d-flex flex-column justify-content-center align-items-center text-center">
-        <h1 className="start-page-title mb-3">Ready for the miracle</h1>
-        <p className="start-page-subtitle">Welcome to our amazing world of generation</p>
+        <h1 className="start-page-title mb-3">Ready to face the power of chatGPT?</h1>
+        <p className="start-page-subtitle">Welcome to the world of content generation</p>
         <div className="start-page-button-container">
           {trail.map((props, index) => (
             <animated.div key={index} style={props}>
-              <Link to={`/${buttons[index].color}`} className={`btn start-page-button start-page-button-${buttons[index].color}`}>
+              <Link to={buttons[index].link} className={`btn start-page-button start-page-button-${buttons[index].color}`}>
                 {buttons[index].text}
               </Link>
             </animated.div>
